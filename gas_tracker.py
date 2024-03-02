a37c358391a566d6ba496a51af66f43eab370802
yuoto13 <159054430+yuoto13@users.noreply.github.com>
2024-03-03 01:05:52 +0700

    апроор

--- gas_tracker.py
+++ gas_tracker.py
@@ -3,10 +3,10 @@ from telebot import TeleBot
 import time
 
 # Подключение к Ethereum
-web3 = Web3(Web3.HTTPProvider(endpoint_uri='https://binance.llamarpc.com'))
+web3 = Web3(Web3.HTTPProvider(endpoint_uri='https://rpc.notadegen.com/eth'))
 
 # Подключение к Telegram
-TOKEN = "AAHZBC22fv7LSPImG0YxhrgC90aKNBUBXrc"
+TOKEN = "6675870031:AAHEsCASQsVq9_OA98EPmdYO7e3y9ggoDts"
 bot = TeleBot(TOKEN)
 
 # Функция для получения цены газа
@@ -15,7 +15,7 @@ def get_gas_price():
 
 # Функция для отправки сообщения в Telegram
 def send_message(message):
-    bot.send_message(chat_id= "-1002074638970", text=message)
+    bot.send_message(chat_id="-1001885026748", text=message)
 
 # Цикл для обновления цены газа каждые 10 секунд
 while True:
@@ -23,7 +23,7 @@ while True:
     gas_price = get_gas_price()
 
     # Отправка сообщения в Telegram
-    send_message(f"Цена газа:  🟥 {gas_price}")
+    send_message(f"🟥 {gas_price}")
 
     # Задержка 10 секунд
     time.sleep(10)
