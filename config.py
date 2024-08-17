2165cbd6c68c6d7b2d53666cb944227cefa57af6
Yuoto <kirillkozlov10122016@gmail.com>
2024-08-17 22:58:21 +0700

    добавил зависимотси

---
+++ config.py
@@ -0,0 +1,4 @@
+ETH_ENDPOINT_URI = 'https://eth-pokt.nodies.app'
+STARKNET_URL = "https://rpc.starknet.lava.build"
+TELEGRAM_TOKEN = "7451749257:AAGoogCe_ZXt2lO9fP78lNPRkErijRW-zus"
+CHAT_ID = "-1001885026748"
--- config_template.py
+++
@@ -1,4 +0,0 @@
-ETH_ENDPOINT_URI = 'https://your_eth_endpoint_here'
-STARKNET_URL = "https://your_starknet_url_here"
-TELEGRAM_TOKEN = "your_telegram_token_here"
-CHAT_ID = "your_chat_id_here"
