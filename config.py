69c409f5f1a1da48e1109064fba524475e229e7f
Yuoto <kirillkozlov10122016@gmail.com>
2024-08-17 23:04:12 +0700

    Removed config.py from tracking

--- config.py
+++
@@ -1,4 +0,0 @@
-ETH_ENDPOINT_URI = 'https://eth-pokt.nodies.app'
-STARKNET_URL = "https://rpc.starknet.lava.build"
-TELEGRAM_TOKEN = "7451749257:AAGoogCe_ZXt2lO9fP78lNPRkErijRW-zus"
-CHAT_ID = "-1001885026748"
